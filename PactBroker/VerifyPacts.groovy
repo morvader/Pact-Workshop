39e70aaa9743007100205ab76c89aff5d3d7f845
Francisco Moreno <francisco.moreno@sngular.com>
2020-10-13 11:23:12 +0200

    linux compatible

--- PactBroker/VerifyPacts.groovy
+++ PactBroker/VerifyPacts.groovy
@@ -6,17 +6,32 @@ pipeline {
         stage('Get project') {
             steps {
                 git branch: '6-PactBroker', url: 'https://github.com/morvader/Pact-Workshop'
-                bat 'npm install'
+                if (isUnix()) {
+                    sh 'npm install'    
+                }
+                else{
+                    bat 'npm install'
+                }
             }
         }
         stage('Create Pacts') {
             steps {
-                bat 'npm run pact-server'
+                if (isUnix()) {
+                    sh 'npm run pact-server'
+                }
+                else {
+                    bat 'npm run pact-server'
+                }
             }
         }
         stage('Can-I-Deploy Server') {
             steps {
-                bat 'npx pact-broker can-i-deploy --pacticipant FilmsProvider --broker-base-url http://localhost:8000 --broker-username pact_workshop --broker-password pact_workshop --latest'
+                if (isUnix()) {
+                    sh 'npx pact-broker can-i-deploy --pacticipant FilmsProvider --broker-base-url http://localhost:8000 --broker-username pact_workshop --broker-password pact_workshop --latest'
+                }
+                else{
+                    bat 'npx pact-broker can-i-deploy --pacticipant FilmsProvider --broker-base-url http://localhost:8000 --broker-username pact_workshop --broker-password pact_workshop --latest'
+                }
             }
         }
     }
