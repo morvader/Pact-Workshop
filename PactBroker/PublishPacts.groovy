c307b5642483fa3b9bf540001edeb7d82e556990
Francisco Moreno <francisco.moreno@sngular.com>
2020-10-13 11:38:23 +0200

    fix unix script

--- PactBroker/PublishPacts.groovy
+++ PactBroker/PublishPacts.groovy
@@ -5,17 +5,38 @@ pipeline {
         stage('Get project') {
             steps {
                 git branch: '6-PactBroker', url: 'https://github.com/morvader/Pact-Workshop'
-                bat 'npm install'
+                script {
+                    if (isUnix()) {
+                        sh 'npm install'
+                    }
+                    else {
+                        bat 'npm install'
+                    }
+                }
             }
         }
         stage('Create Pacts') {
             steps {
-                bat 'npm run generate-pact-client'
+                script {
+                    if (isUnix()) {
+                        sh 'npm run generate-pact-client'
+                    }
+                    else {
+                        bat 'npm run generate-pact-client'
+                    }
+                }
             }
         }
         stage('Publish Pacts') {
             steps {
-                bat 'npm run publish-pacts-Broker'
+                script {
+                    if (isUnix()) {
+                        sh 'npm run publish-pacts-Broker'
+                    }
+                    else {
+                        bat 'npm run publish-pacts-Broker'
+                    }
+                }
             }
         }
     }
--- PactBroker/VerifyPacts.groovy
+++ PactBroker/VerifyPacts.groovy
@@ -6,31 +6,37 @@ pipeline {
         stage('Get project') {
             steps {
                 git branch: '6-PactBroker', url: 'https://github.com/morvader/Pact-Workshop'
-                if (isUnix()) {
-                    sh 'npm install'    
-                }
-                else{
-                    bat 'npm install'
+                script {
+                    if (isUnix()) {
+                        sh 'npm install'
+                    }
+                    else {
+                        bat 'npm install'
+                    }
                 }
             }
         }
         stage('Create Pacts') {
             steps {
-                if (isUnix()) {
-                    sh 'npm run pact-server'
-                }
-                else {
-                    bat 'npm run pact-server'
+                script {
+                    if (isUnix()) {
+                        sh 'npm run pact-server'
+                    }
+                    else {
+                        bat 'npm run pact-server'
+                    }
                 }
             }
         }
         stage('Can-I-Deploy Server') {
             steps {
-                if (isUnix()) {
-                    sh 'npx pact-broker can-i-deploy --pacticipant FilmsProvider --broker-base-url http://localhost:8000 --broker-username pact_workshop --broker-password pact_workshop --latest'
-                }
-                else{
-                    bat 'npx pact-broker can-i-deploy --pacticipant FilmsProvider --broker-base-url http://localhost:8000 --broker-username pact_workshop --broker-password pact_workshop --latest'
+                script {
+                    if (isUnix()) {
+                        sh 'npx pact-broker can-i-deploy --pacticipant FilmsProvider --broker-base-url http://localhost:8000 --broker-username pact_workshop --broker-password pact_workshop --latest'
+                    }
+                    else {
+                        bat 'npx pact-broker can-i-deploy --pacticipant FilmsProvider --broker-base-url http://localhost:8000 --broker-username pact_workshop --broker-password pact_workshop --latest'
+                    }
                 }
             }
         }
